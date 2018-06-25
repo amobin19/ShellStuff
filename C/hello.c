f09296231cebd1f37258f046516cf75c6ee65217
amobin19 <aymamob@gmail.com>
2018-06-25 09:11:34 -0400

    unifinished hello.c

--- C/hello.c
+++ C/hello.c
@@ -1,5 +1,18 @@
 #include <stdio.h>
+int numSwap(int a, int b);
+
+int numSwap(a, b){
+	int a;
+	int b;
+	a = a + b;
+	b = a - b;
+	a = a - b;
+	return(a, b)
+}
+
 int main(){
+	ret = numSwap(5, 4)
+	printf(ret\n);
 	printf("Hello, Ayman!\n");
 	return 0;
 }
