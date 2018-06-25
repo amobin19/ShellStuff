c5068adc34da4a67bf79ee91a81c02b61eee79fa
root <root@localhost.localdomain>
2018-06-25 12:01:37 -0400

    improvement

--- C/hello.c
+++ C/hello.c
@@ -1,18 +1,44 @@
 #include <stdio.h>
-int numSwap(int a, int b);
+int min(int a, int b);
 
-int numSwap(a, b){
-	int a;
-	int b;
+void numSwap(int a, int b);
+
+void numSwap2(int c, int d);
+
+int main(){
+	int x = 10;
+	int y = 5;
+	//numSwap2(x, y);
+	int temp;
+	temp = x;
+	x = y;
+	y = temp;
+	printf("After swap: x = %d and y = %d\n", x, y);
+	return 0;
+}
+
+void numSwap(int a, int b){
 	a = a + b;
 	b = a - b;
 	a = a - b;
-	return(a, b)
+	return;
 }
 
-int main(){
-	ret = numSwap(5, 4)
-	printf(ret\n);
-	printf("Hello, Ayman!\n");
-	return 0;
+void numSwap2(int c, int d){
+	int temp;
+	temp = c;
+	c = d;
+	d = temp;
+	return;
+}
+
+int min(int a, int b){
+	int final;
+	if(a < b){
+		final = a;
+	}
+	else{
+		final = b;
+	}
+	return final;
 }
